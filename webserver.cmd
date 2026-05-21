8d3e15ab78a0012e55529c01975c9c73420fb503
Ajay <ajay.msn3@gmail.com>
2026-05-21 21:41:22 +0530

    Add blank lines for improved readability in webserver.cmd

--- webserver.cmd
+++ webserver.cmd
@@ -43,10 +43,12 @@ if "%IP%"=="" (
 
 set URL=http://%IP%:%PORT%
 
+
 echo.
 echo Sharing: %SHARE_PATH%
 echo URL: %URL%
 echo.
+
 :: Start server in background
 start /b cmd /c "cd /d "%SHARE_PATH%" && python -m http.server %PORT%"
 
